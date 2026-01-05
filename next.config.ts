7e34b2df5560e5fd2ca5a7f6f306dddd28465e7d
zhangzhuo <zhangzhuo@unipus.cn>
2026-01-05 17:28:18 +0800

    feat: 更改构建

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,34 @@
 import type { NextConfig } from "next";
+import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    // 配置turbopack根目录
+    root: path.join('./'),
+    // 配置svg加载器
+    rules: {
+      '*.svg': {
+        loaders: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              dimensions: false,
+              typescript: true,
+            },
+          },
+        ],
+        as: '*.js',
+      },
+    },
+  },
 };
 
 if (process.env.NODE_ENV === 'production') {
   nextConfig.output = 'export'
+  nextConfig.trailingSlash = true
   nextConfig.distDir = 'docs'
+  nextConfig.basePath = '/z-ui'
 }
 
 export default nextConfig;
